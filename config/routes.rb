51e9a14ae6cec28910ae12e108cbfabf41b4b933
Annika Lang <Lang-annika@t-online.de>
2019-08-27 01:11:00 +0200

    setup of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  # GET /profile users controller profile action
+  get '/profile', to: 'users#profile'
+  # resources for parties, only [:new, :create, :show]
+  resources :parties, only: [:new, :create, :show] do
+    resources :guests, only: [:index]
+  end
+
+  resources :playlists, only: [:show, :edit] do
+    resources :tracks, only: [:index]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
