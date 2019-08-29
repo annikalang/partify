24d5d1588920d48c7c0a3aac67bf5d8a5a0bf8f0
Annika Lang <Lang-annika@t-online.de>
2019-08-29 18:14:43 +0200

    added average, default image on party show page

--- app/jobs/spotify_job.rb
+++ app/jobs/spotify_job.rb
@@ -7,8 +7,8 @@ class SpotifyJob < ApplicationJob
     # create tracks in DB using playlist_id as playlist
     spotify_user = RSpotify::User.find(spotify_user_id)
 
-    spotify_user.playlists.first.tracks.each do |track|
-      #playlist.tracks.each do |track|
+    spotify_user.playlists.each do |playlist|
+      playlist.tracks.each do |track|
         Track.create(
           title: track.name,
           playlist_id: playlist_id,
@@ -18,9 +18,12 @@ class SpotifyJob < ApplicationJob
           valence: track.audio_features.valence,
           spotify_id: track.id,
           popularity: track.popularity,
-          image: track.artists.first.images.last["url"]
+          image: track.artists.first.images.last["url"],
+          artist: track.artists.first.name,
+          genre: track.artists.first.genres.first
         )
       end
+    end
   end
 end
 
