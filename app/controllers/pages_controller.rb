c33089289c295eb8024d1bd025dd2329a410fdf6
Annika Lang <Lang-annika@t-online.de>
2019-09-03 09:39:28 +0200

    added onboarding to be skipped by authentication of user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :onboarding]
 
   def home
+  end
+
+  def onboarding
   end
 
   def components
